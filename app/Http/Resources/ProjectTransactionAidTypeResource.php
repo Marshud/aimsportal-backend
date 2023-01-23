fd760d34bc3f29b2ff0d3e797b182626054d27cd
Raymond <raymond.byaruhanga@ingenious.build>
2023-01-23 12:42:41 +0300

    update transactions

---
+++ app/Http/Resources/ProjectTransactionAidTypeResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectTransactionAidTypeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'iati_code' => $this->code,
+            'iati_vocabulary' => $this->vocabulary,
+            'code' => $this->iati_code()->name ?? null,
+            'vocabulary' => $this->iati_vocabulary()->name ?? null,
+        ];
+    }
+}
